63dfb5473fa76e9f6ce99fc8857126e9cb14f832
Loïc Ballanger <ballanger.loic@gmail.com>
2022-05-12 19:24:32 +0200

    ✨ Server and router initialization

--- back/index.js
+++ back/index.js
@@ -1,1 +1,16 @@
-console.log('Server test');
+require('dotenv').config();
+const express = require('express');
+const router = require('./app/router');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('test');
+});
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Server started on http://localhost:${port}`);
+});
