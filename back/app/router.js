8669c7c8fe52cf2add81726238ebf73c0c78edaa
Loïc Ballanger <ballanger.loic@gmail.com>
2022-06-01 17:58:13 +0200

    ✨ Add routes for find the articles

--- back/app/router.js
+++ back/app/router.js
@@ -4,6 +4,7 @@ const router = Router();
 
 const userController = require('./controllers/userController');
 const authController = require('./controllers/authController');
+const articleController = require('./controllers/articleController');
 
 /** ********************** USER *********************** */
 router.get('/api/user', userController.user);
@@ -11,5 +12,9 @@ router.get('/api/user', userController.user);
 /** ********************** AUTH *********************** */
 router.post('/api/auth/login', authController.login);
 router.post('/api/auth/refresh-token', authController.refreshToken);
+
+/** ********************** ARTICLE *********************** */
+
+router.get('/api/articles', articleController.findAll);
 
 module.exports = router;
