a94267f1062459507f9f75f42f45ef2524c2c907
Loïc Ballanger <ballanger.loic@gmail.com>
2022-06-02 14:34:51 +0200

    ✨ Adding the slice for articles

--- front/src/store/store.js
+++ front/src/store/store.js
@@ -1,8 +1,11 @@
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
 import userSlice from "../slices/userSlice";
+import articlesSlice from "../slices/articleSlice";
 
 const reducer = combineReducers({
   userSlice,
+  articlesSlice,
 });
 
 const store = configureStore({
