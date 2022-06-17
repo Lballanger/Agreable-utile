7d28656483d1e4cd95409d5fc3dface67375292d
Loïc Ballanger <ballanger.loic@gmail.com>
2022-06-18 00:17:36 +0200

    ✨ Adding a structure and style for shop component

--- front/src/components/Shop/Shop.jsx
+++ front/src/components/Shop/Shop.jsx
@@ -1,9 +1,215 @@
 import "./Shop.scss";
+import exampleimage from "../../assets/img/exampleimage3.jpg";
+import exampleimage4 from "../../assets/img/exampleimage4.jpg";
+import exampleimage5 from "../../assets/img/exampleimage5.jpg";
+import exampleimage6 from "../../assets/img/exampleimage6.jpg";
 
 function Shop() {
   return (
     <div className="shop">
-      <h2 className="shop__title">Boutique</h2>
+      <div className="shop__title-container">
+        <h2 className="shop__title-container__title">Boutique</h2>
+        <div className="shop__title-container__img-container">
+          <img
+            className="shop__title-container__img-container__img"
+            src={exampleimage}
+            alt=""
+          />
+        </div>
+      </div>
+      <div className="shop__main-container">
+        <div className="shop__main-container__product">
+          <div className="shop__main-container__product__img-container">
+            <img
+              src={exampleimage4}
+              alt=""
+              className="shop__main-container__product__img-container__img"
+            />
+          </div>
+          <div className="shop__main-container__product__infos-container">
+            <h2 className="shop__main-container__product__infos-container__infos">
+              <div className="shop__main-container__product__infos-container__infos__title">
+                Lingettes lavable
+              </div>
+              <div className="shop__main-container__product__infos-container__infos__description">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+                inventore explicabo corrupti veritatis. Quasi eum veritatis
+              </div>
+            </h2>
+            <div className="shop__main-container__product__infos-container__price">
+              17,29€
+            </div>
+          </div>
+        </div>
+
+        <div className="shop__main-container__product">
+          <div className="shop__main-container__product__img-container">
+            <img
+              src={exampleimage5}
+              alt=""
+              className="shop__main-container__product__img-container__img"
+            />
+          </div>
+          <div className="shop__main-container__product__infos-container">
+            <h2 className="shop__main-container__product__infos-container__infos">
+              <div className="shop__main-container__product__infos-container__infos__title">
+                Lingettes lavable
+              </div>
+              <div className="shop__main-container__product__infos-container__infos__description">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+                inventore explicabo corrupti veritatis. Quasi eum veritatis
+              </div>
+            </h2>
+            <div className="shop__main-container__product__infos-container__price">
+              17,29€
+            </div>
+          </div>
+        </div>
+
+        <div className="shop__main-container__product">
+          <div className="shop__main-container__product__img-container">
+            <img
+              src={exampleimage6}
+              alt=""
+              className="shop__main-container__product__img-container__img"
+            />
+          </div>
+          <div className="shop__main-container__product__infos-container">
+            <h2 className="shop__main-container__product__infos-container__infos">
+              <div className="shop__main-container__product__infos-container__infos__title">
+                Lingettes lavable
+              </div>
+              <div className="shop__main-container__product__infos-container__infos__description">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+                inventore explicabo corrupti veritatis. Quasi eum veritatis
+              </div>
+            </h2>
+            <div className="shop__main-container__product__infos-container__price">
+              17,29€
+            </div>
+          </div>
+        </div>
+
+        <div className="shop__main-container__product">
+          <div className="shop__main-container__product__img-container">
+            <img
+              src={exampleimage}
+              alt=""
+              className="shop__main-container__product__img-container__img"
+            />
+          </div>
+          <div className="shop__main-container__product__infos-container">
+            <h2 className="shop__main-container__product__infos-container__infos">
+              <div className="shop__main-container__product__infos-container__infos__title">
+                Lingettes lavable
+              </div>
+              <div className="shop__main-container__product__infos-container__infos__description">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+                inventore explicabo corrupti veritatis. Quasi eum veritatis
+              </div>
+            </h2>
+            <div className="shop__main-container__product__infos-container__price">
+              17,29€
+            </div>
+          </div>
+        </div>
+
+        <div className="shop__main-container__product">
+          <div className="shop__main-container__product__img-container">
+            <img
+              src={exampleimage}
+              alt=""
+              className="shop__main-container__product__img-container__img"
+            />
+          </div>
+          <div className="shop__main-container__product__infos-container">
+            <h2 className="shop__main-container__product__infos-container__infos">
+              <div className="shop__main-container__product__infos-container__infos__title">
+                Lingettes lavable
+              </div>
+              <div className="shop__main-container__product__infos-container__infos__description">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+                inventore explicabo corrupti veritatis. Quasi eum veritatis
+              </div>
+            </h2>
+            <div className="shop__main-container__product__infos-container__price">
+              17,29€
+            </div>
+          </div>
+        </div>
+
+        <div className="shop__main-container__product">
+          <div className="shop__main-container__product__img-container">
+            <img
+              src={exampleimage}
+              alt=""
+              className="shop__main-container__product__img-container__img"
+            />
+          </div>
+          <div className="shop__main-container__product__infos-container">
+            <h2 className="shop__main-container__product__infos-container__infos">
+              <div className="shop__main-container__product__infos-container__infos__title">
+                Lingettes lavable
+              </div>
+              <div className="shop__main-container__product__infos-container__infos__description">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+                inventore explicabo corrupti veritatis. Quasi eum veritatis
+              </div>
+            </h2>
+            <div className="shop__main-container__product__infos-container__price">
+              17,29€
+            </div>
+          </div>
+        </div>
+
+        <div className="shop__main-container__product">
+          <div className="shop__main-container__product__img-container">
+            <img
+              src={exampleimage}
+              alt=""
+              className="shop__main-container__product__img-container__img"
+            />
+          </div>
+          <div className="shop__main-container__product__infos-container">
+            <h2 className="shop__main-container__product__infos-container__infos">
+              <div className="shop__main-container__product__infos-container__infos__title">
+                Lingettes lavable
+              </div>
+              <div className="shop__main-container__product__infos-container__infos__description">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+                inventore explicabo corrupti veritatis. Quasi eum veritatis
+              </div>
+            </h2>
+            <div className="shop__main-container__product__infos-container__price">
+              17,29€
+            </div>
+          </div>
+        </div>
+
+        <div className="shop__main-container__product">
+          <div className="shop__main-container__product__img-container">
+            <img
+              src={exampleimage}
+              alt=""
+              className="shop__main-container__product__img-container__img"
+            />
+          </div>
+          <div className="shop__main-container__product__infos-container">
+            <h2 className="shop__main-container__product__infos-container__infos">
+              <div className="shop__main-container__product__infos-container__infos__title">
+                Lingettes lavable
+              </div>
+              <div className="shop__main-container__product__infos-container__infos__description">
+                Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni
+                inventore explicabo corrupti veritatis. Quasi eum veritatis
+              </div>
+            </h2>
+            <div className="shop__main-container__product__infos-container__price">
+              17,29€
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
