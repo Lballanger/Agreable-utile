d4a336fd18be7b16fdf777257e2a82d7687b3c93
Loïc Ballanger <ballanger.loic@gmail.com>
2022-06-20 16:57:16 +0200

    ✨ Adding a route to find the achievements and a route to an achievement

--- back/app/router.js
+++ back/app/router.js
@@ -5,6 +5,7 @@ const router = Router();
 const userController = require('./controllers/userController');
 const authController = require('./controllers/authController');
 const articleController = require('./controllers/articleController');
+const achievementController = require('./controllers/achievementController');
 
 /** ********************** USER *********************** */
 router.get('/api/user', userController.user);
@@ -18,5 +19,10 @@ router.post('/api/auth/refresh-token', authController.refreshToken);
 
 router.get('/api/articles', articleController.findAll);
 router.get('/api/article/:id', articleController.findOneById);
+
+/** ********************** ACHIEVEMENT *********************** */
+
+router.get('/api/achievements', achievementController.findAll);
+router.get('/api/achievement/:id', achievementController.findOneById);
 
 module.exports = router;
