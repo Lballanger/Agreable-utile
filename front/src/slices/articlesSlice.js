e0bf2c80dce7b96735353ffb93f1f10b4b7804a5
Loïc Ballanger <ballanger.loic@gmail.com>
2022-06-22 12:55:06 +0200

    🔨 Add addToCart méthod in articlesSlice reducer

--- front/src/slices/articlesSlice.js
+++ front/src/slices/articlesSlice.js
@@ -4,13 +4,36 @@ const articlesSlice = createSlice({
   name: "articles",
   initialState: {
     articles: null,
+    cart: [],
   },
   reducers: {
     setArticlesData(state, { payload }) {
       state.articles = payload;
     },
+
+    addToCart(state, { payload }) {
+      const params = payload;
+
+      const foundArticle = state.cart.find(
+        (article) => article.id === payload.id,
+      );
+
+      if (foundArticle !== undefined) {
+        foundArticle.quantity += 1;
+        const result = state.cart.filter(
+          (article) => article.id !== foundArticle.id,
+        );
+        state.cart = result;
+        state.cart.push(foundArticle);
+        localStorage.setItem("CART", JSON.stringify(state.cart));
+      } else {
+        params.quantity = 1;
+        state.cart.push(params);
+        localStorage.setItem("CART", JSON.stringify(state.cart));
+      }
+    },
   },
 });
 
-export const { setArticlesData } = articlesSlice.actions;
+export const { setArticlesData, addToCart } = articlesSlice.actions;
 export default articlesSlice.reducer;
