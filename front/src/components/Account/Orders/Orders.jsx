ed76a577d08154da4a84bad2704652d068d69595
Loïc Ballanger <ballanger.loic@gmail.com>
2022-10-10 17:46:17 +0200

    Update button to view order

--- front/src/components/Account/Orders/Orders.jsx
+++ front/src/components/Account/Orders/Orders.jsx
@@ -22,56 +22,62 @@ function Orders() {
         {orders?.map((order) => (
           <div className="orders__container__order" key={order.order_number}>
             <div className="orders__container__order__top-container">
-              <div className="orders__container__order__top-container__img-container">
+              <div className="orders__container__order__top-container__articles">
                 {order.articles.map((article) => (
-                  <img
-                    className="orders__container__order__top-container__img-container__img"
-                    key={article.id}
-                    src={`/src/assets/img/shop/articles/${article.image[0]}`}
-                    alt=""
-                    srcSet=""
-                  />
+                  <div className="orders__container__order__top-container__articles__img-container">
+                    <img
+                      className="orders__container__order__top-container__articles__img-container__img"
+                      key={article.id}
+                      src={`/src/assets/img/shop/articles/${article.image[0]}`}
+                      alt=""
+                      srcSet=""
+                    />
+                  </div>
                 ))}
-              </div>
-
-              <div className="orders__container__order__top-container__button-container">
-                <Link to={`/order/${order.order_number}`}>
-                  Voir la commande
-                </Link>
               </div>
             </div>
             <div className="orders__container__order__bottom-container">
-              <div className="orders__container__order__bottom-container__number-container">
-                <h4 className="orders__container__order__bottom-container__number-container__title">
-                  Numéro de commande
-                </h4>
-                <div className="orders__container__order__bottom-container__number-container__number">
-                  {order.order_number}
+              <div className="orders__container__order__bottom-container__info">
+                <div className="orders__container__order__bottom-container__info__container">
+                  <h4 className="orders__container__order__bottom-container__info__container__title">
+                    Numéro de commande
+                  </h4>
+                  <div className="orders__container__order__bottom-container__info__container__content">
+                    {order.order_number}
+                  </div>
+                </div>
+                <div className="orders__container__order__bottom-container__info__container">
+                  <h4 className="orders__container__order__bottom-container__info__container__title">
+                    Date de la commande
+                  </h4>
+                  <div className="orders__container__order__bottom-container__info__container__content">
+                    {new Date(order.created_at).toLocaleDateString("fr")}
+                  </div>
+                </div>
+                <div className="orders__container__order__bottom-container__info__container">
+                  <h4 className="orders__container__order__bottom-container__info__container__title">
+                    Total
+                  </h4>
+                  <div className="orders__container__order__bottom-container__info__container__content">
+                    103.50 €
+                  </div>
+                </div>
+                <div className="orders__container__order__bottom-container__info__container">
+                  <h4 className="orders__container__order__bottom-container__info__container__title">
+                    État de la commande
+                  </h4>
+                  <div className="orders__container__order__bottom-container__info__container__content">
+                    --
+                  </div>
                 </div>
               </div>
-              <div className="orders__container__order__bottom-container__number-container">
-                <h4 className="orders__container__order__bottom-container__number-container__title">
-                  Date de la commande
-                </h4>
-                <div className="orders__container__order__bottom-container__number-container__number">
-                  {new Date(order.created_at).toLocaleDateString("fr")}
-                </div>
-              </div>
-              <div className="orders__container__order__bottom-container__number-container">
-                <h4 className="orders__container__order__bottom-container__number-container__title">
-                  Total
-                </h4>
-                <div className="orders__container__order__bottom-container__number-container__number">
-                  103.50 €
-                </div>
-              </div>
-              <div className="orders__container__order__bottom-container__number-container">
-                <h4 className="orders__container__order__bottom-container__number-container__title">
-                  État de la commande
-                </h4>
-                <div className="orders__container__order__bottom-container__number-container__number">
-                  --
-                </div>
+              <div className="orders__container__order__bottom-container__link-container">
+                <Link
+                  className="orders__container__order__bottom-container__link-container__link"
+                  to={`/order/${order.order_number}`}
+                >
+                  Voir la commande
+                </Link>
               </div>
             </div>
           </div>
