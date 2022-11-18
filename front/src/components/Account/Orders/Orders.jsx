a8e4c2410c087e7be53046c361dc4c940f772fa8
Loïc Ballanger <ballanger.loic@gmail.com>
2022-11-18 12:58:40 +0100

    Updating the paths for production

--- front/src/components/Account/Orders/Orders.jsx
+++ front/src/components/Account/Orders/Orders.jsx
@@ -32,7 +32,7 @@ function Orders() {
                       src={
                         import.meta.env.VITE_NODE_ENV !== "production"
                           ? `/src/assets/img/shop/articles/${article.image[0]}`
-                          : dynamicUrl(article.image[0])}
+                          : dynamicUrl(article.image[0])
                       }
                       alt=""
                       srcSet=""
