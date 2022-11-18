52174f43b82c34565d2e14c99b027b83388aa114
Loïc Ballanger <ballanger.loic@gmail.com>
2022-11-18 13:04:19 +0100

    Updating the paths for production

--- front/src/utils/viteURL.js
+++ front/src/utils/viteURL.js
@@ -1,6 +1,6 @@
 // The function is used so that Vite can include the assets during production
 const dynamicUrl = (name) => {
-  return new URL(`./dir/${name}.png`, import.meta.url).href;
+  return new URL(`../assets/img/shop/articles/${name}`, import.meta.url).href;
 };
 
 export default dynamicUrl;
