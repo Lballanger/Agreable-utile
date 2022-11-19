c52200fb5662f627267590c8b510348b6dc19ce1
Loïc Ballanger <ballanger.loic@gmail.com>
2022-11-19 13:37:16 +0100

    Updating the paths for production

--- front/vite.config.js
+++ front/vite.config.js
@@ -12,4 +12,26 @@ export default defineConfig({
       },
     },
   },
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: "assets/img/shop/articles/[name]-[hash].js",
+        entryFileNames: "assets/img/shop/articles/[name]-[hash].js",
+
+        assetFileNames: ({ name }) => {
+          if (/\.(gif|jpe?g|png|svg|jpg)$/.test(name ?? "")) {
+            return "assets/img/shop/articles/[name]-[hash][extname]";
+          }
+
+          if (/\.css$/.test(name ?? "")) {
+            return "assets/css/[name]-[hash][extname]";
+          }
+
+          // default value
+          // ref: https://rollupjs.org/guide/en/#outputassetfilenames
+          return "assets/[name]-[hash][extname]";
+        },
+      },
+    },
+  },
 });
