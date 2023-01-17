4f2a415bf5c1200fbb5bfbc8e14a1f4b3a17f578
Loïc Ballanger <ballanger.loic@gmail.com>
2023-01-17 17:43:42 +0100

    Added the create method, and added support for uploading images to cloudinary

--- back/app/controllers/articleController.js
+++ back/app/controllers/articleController.js
@@ -1,6 +1,45 @@
+const fs = require("fs");
 const Article = require("../models/article");
+const cloudinary = require("../services/cloudinaryService");
 
 const articleController = {
+  create: async (request, response) => {
+    try {
+      const { name, description, price, quantity, categoryId } = request.body;
+
+      const { files } = request;
+
+      // Upload the image to Cloudinary
+      const uploadedImages = await Promise.all(
+        files.map(async (file) => {
+          const result = await cloudinary.uploader.upload(file.path);
+          return result.public_id;
+        }),
+      );
+
+      files.forEach((file) => {
+        fs.unlink(file.path, (err) => {
+          if (err) throw err;
+          console.log(`Successfully deleted ${file.path}`);
+        });
+      });
+
+      // Create a new article
+      const article = await new Article({
+        name,
+        description,
+        image: uploadedImages,
+        price_wt: price,
+        quantity,
+        category_id: categoryId,
+      }).create();
+      response.status(201).json(article);
+    } catch (error) {
+      console.log(error);
+      response.status(500).json(error.message);
+    }
+  },
+
   findAll: async (request, response) => {
     try {
       const articles = await Article.findAll();
