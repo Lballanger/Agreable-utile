35234fa307b6a5dccfbbd864d6c54f7ff6e2cde9
Loïc Ballanger <ballanger.loic@gmail.com>
2023-01-17 17:45:16 +0100

    Adding the create method of the article model and adding the quantity and status

--- back/app/models/article.js
+++ back/app/models/article.js
@@ -9,10 +9,44 @@ class Article {
     }
   }
 
+  async create() {
+    try {
+      const { rows } = await client.query(
+        `INSERT INTO 
+          private.article (
+            name, 
+            description, 
+            image, 
+            price_wt,
+            quantity, 
+            category_id
+          ) VALUES (
+            $1, 
+            $2, 
+            $3, 
+            $4, 
+            $5,
+            $6
+          ) RETURNING *`,
+        [
+          this.name,
+          this.description,
+          this.image,
+          this.price_wt,
+          this.quantity,
+          this.category_id,
+        ],
+      );
+      return new Article(rows[0]);
+    } catch (error) {
+      return new Error(error.detail ? error.detail : error.message);
+    }
+  }
+
   static async findAll() {
     try {
       const { rows } = await client.query(
-        "SELECT article.id AS article_id, description, image, article.name AS article_name, price_wt, category.name AS category_name, category.id AS category_id FROM private.article  JOIN private.category ON article.category_id= category.id;",
+        "SELECT article.id AS article_id, description, image, article.name AS article_name, price_wt, quantity, status, created_at, updated_at, category.name AS category_name, category.id AS category_id FROM private.article  JOIN private.category ON article.category_id= category.id;",
       );
       return rows.map((article) => new Article(article));
     } catch (error) {
