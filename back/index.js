8ace7de22d07fefc711a7ab62cbc027b504c0784
Loïc Ballanger <ballanger.loic@gmail.com>
2023-01-27 18:48:00 +0100

    Add admin panel address for the cors policy

--- back/index.js
+++ back/index.js
@@ -1,3 +1,5 @@
+// index.js
+
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
@@ -6,6 +8,18 @@ const router = require("./app/router");
 
 const app = express();
 const port = process.env.NODE_ENV === "production" ? 5000 : process.env.PORT;
+
+app.use(
+  cors({
+    origin:
+      process.env.NODE_ENV === "production"
+        ? [
+            "https://lagreable-utile-r92cy.ondigitalocean.app",
+            "https://lagreable-utile-admin-arzg3.ondigitalocean.app/",
+          ]
+        : ["http://127.0.0.1:5173", "http://127.0.0.1:5174"],
+  }),
+);
 
 app.use((req, res, next) => {
   if (req.originalUrl === "/api/webhook") {
@@ -16,15 +30,6 @@ app.use((req, res, next) => {
 });
 
 app.use(express.urlencoded({ extended: true }));
-
-app.use(
-  cors({
-    origin:
-      process.env.NODE_ENV === "production"
-        ? "https://lagreable-utile-r92cy.ondigitalocean.app"
-        : ["http://127.0.0.1:5173", "http://127.0.0.1:5174"],
-  }),
-);
 
 app.use(router);
 
