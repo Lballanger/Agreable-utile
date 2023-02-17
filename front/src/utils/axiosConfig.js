94175c6bd29652fe235722351be117cf63abc16e
Loïc Ballanger <ballanger.loic@gmail.com>
2023-02-17 09:36:48 +0100

    Fix url

--- front/src/utils/axiosConfig.js
+++ front/src/utils/axiosConfig.js
@@ -4,7 +4,7 @@ const instance = axios.create({
   baseURL:
     import.meta.env.VITE_NODE_ENV !== "production"
       ? "http://localhost:3500/api/"
-      : "https://lballanger.fr/api/lagreable/",
+      : "https://lballanger.fr/lagreable/api/",
   headers: {
     "Content-Type": "application/json",
   },
